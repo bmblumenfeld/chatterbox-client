da90fde3df1dd879a1e1445859696643fd02924d
bmblumenfeld <bmblumenfeld@gmail.com>
2018-01-27 16:42:29 -0800

    Implemented room selection

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,23 @@
 
 var app = {
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
-  messages: []
+  messages: [],
+  rooms: {},
+  room: null
+};
+app.getParams = function (name) {
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  return results === null ? 0 : results[1];
 };
 app.init = function() {
-  setInterval(this.fetch.bind(this), 1000);
+  var room = this.getParams('room'); 
+  console.log('room: ' + room);
+  if (room) {
+    this.room = room;
+    setInterval(this.fetchRoom.bind(this), 1000);
+  } else {
+    setInterval(this.fetch.bind(this), 1000);
+  }
 };
 app.send = function(message) {
   $.ajax({
@@ -23,33 +36,14 @@ app.send = function(message) {
   });
 };
 app.fetch = function() {
-  // var date = new Date('August 19, 1975 23:15:30 UTC');
-  // date = date.toJSON();
-  // var url = this.server + '?where={"createdAt":{"$lt":' + date + '}}';
-  // var params = {
-  //   "createdAt": {
-  //     "$all": {}
-  //   }
-  // };
-  // var params = {
-  //   "$all": {
-  //   }
-  // };
-  //url += JSON.stringify(params);
-  // console.log(url);
   $.ajax({
     url: this.server,
     type: 'GET',
     dataType: 'json',
     data: 'order=-createdAt',
-    //success: ({results}) => {
     success: ({results}) => {
-      //console.log(data); 
-
-      console.log(results);
+      //console.log(results);
       var newestMsgId = this.messages.length ? this.messages[this.messages.length - 1].objectId : null;
-      // console.log(this.messages);
-      // console.log(newestMsgId);
       var oldestNewMsgIndex = 0;
       if (!newestMsgId) {
         oldestNewMsgIndex = results.length;
@@ -61,6 +55,15 @@ app.fetch = function() {
       $chats = $('#chats');
       for (var i = oldestNewMsgIndex - 1; i >= 0; i--) {
         this.messages.push(results[i]);
+        var room = this.messages[this.messages.length - 1].roomname;
+        //console.log(room);
+        if (room && !this.rooms[room]) {
+          
+          this.rooms[room] = room;
+          // console.log(this)
+          //console.log(room);
+          this.renderRoom(room);
+        }
         //gets date in readable format 
         var date = new Date(this.messages[this.messages.length - 1].createdAt);
         date = date.toString();
@@ -68,7 +71,7 @@ app.fetch = function() {
         var message = {
           username: this.messages[this.messages.length - 1].username,
           text: this.messages[this.messages.length - 1].text,
-          roomname: this.messages[this.messages.length - 1].roomname,
+          roomname: room,
           date: date   
         };
         this.renderMessage(message);
@@ -76,13 +79,67 @@ app.fetch = function() {
     }
   });
 };
+app.fetchRoom = function() {
+  $.ajax({
+    url: this.server + '?where={"roomname":"' + this.room + '"}',
+    type: 'GET',
+    dataType: 'json',
+    data: 'order=-createdAt',
+    success: ({results}) => {
+      //console.log(results);
+      var newestMsgId = this.messages.length ? this.messages[this.messages.length - 1].objectId : null;
+      var oldestNewMsgIndex = 0;
+      if (!newestMsgId) {
+        oldestNewMsgIndex = results.length;
+      } else {
+        while (results[oldestNewMsgIndex].objectId !== newestMsgId) {
+          oldestNewMsgIndex++;
+        }
+      }
+      $chats = $('#chats');
+      for (var i = oldestNewMsgIndex - 1; i >= 0; i--) {
+        this.messages.push(results[i]);
+        var room = this.messages[this.messages.length - 1].roomname;
+        //console.log(room);
+        if (room && !this.rooms[room]) {
+          
+          this.rooms[room] = room;
+          // console.log(this)
+          //console.log(room);
+          this.renderRoom(room);
+        }
+        //gets date in readable format 
+        var date = new Date(this.messages[this.messages.length - 1].createdAt);
+        date = date.toString();
+        date = date.slice(0, 15);
+        var message = {
+          username: this.messages[this.messages.length - 1].username,
+          text: this.messages[this.messages.length - 1].text,
+          roomname: room,
+          date: date   
+        };
+        this.renderMessage(message);
+      }
+    }
+  });
+};
+app.renderRoom = function (roomname) {
+  //console.log('asdfasdgasdgasdgasdgd');
+  //console.log('asdfasdgasdgasdgasdgd: ' + roomname);
+  roomname = roomname.slice(0, 25);
+  var $rooms = $('.roomSelect');
+  var $room = $('<a href = "index.html?room=' + roomname + '" class = "room">' + roomname + '</a></br>');
+  $room.text(roomname);
+  $room.prependTo($rooms);
+};
 app.renderMessage = function ({username, text, roomname, date}) {
   var $chats = $('#chats');
+  
   var $chat = $('<div class = "chat">\
   <header class = "header"><div class = "username"></div><div class = "date"></div><div class = "roomname"></div></header><div class = "msg"></div></div>');
   $chat.find('.username').text(username);
   $chat.find('.date').text(date);
-  $chat.find('.msg').text(text);
+  $chat.find('.msg').text(text.slice(0, 160));
   $chat.find('.roomname').text(roomname);
   $chat.prependTo($chats);  
     
@@ -90,27 +147,31 @@ app.renderMessage = function ({username, text, roomname, date}) {
 app.clearMessages = function () {
   $('#chats').empty();  
 };
-
-app.renderRoom = function () {
-  var $rooms = $('#roomSelect');
-  var $room = $('<div class = "room"></div>');
-  $rooms.append($room);  
-};
 app.handleUsernameClick = function () {
-  alert("you really made a friend!");  
+  console.log("you really made a friend!");  
+};
+app.handleSubmit = function () {
+  var roomname = this.room === null ? 'home' : this.room;
+  var msg = {
+    username: 'pandasrcool',
+    text: $('#message').val(),
+    roomname: roomname
+  };
+  msg = JSON.stringify(msg);
+  app.send(msg);
 };
 $(document).ready(function() {
   $('#submit').on('click', function(event) {
-    var msg = {
-      username: 'KevinLamFan',
-      text: $('#message').val(),
-      roomname: 'All Kevin All The Time'
-    };
-    msg = JSON.stringify(msg);
-    app.send(msg);
+    app.handleSubmit();
+  });
+  $('body').on('keypress', function(e) {
+    var code = e.keyCode || e.which;
+    if (code == 13) {
+      app.handleSubmit();
+    }
   });
   $('#chats').on('click','.username', function(event) {
-    app.handleUsernameClick.bind(window);
+    app.handleUsernameClick.call(window);
   }); 
 });
 app.init();
